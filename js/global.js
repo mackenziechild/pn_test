12a73198feb4c09384e8198cbd2340d474c86725
Mackenzie Child <mackenzie.child@gmail.com>
2013-05-21 10:46:47 -0600

    Initial Commit

---
+++ js/global.js
@@ -0,0 +1,54 @@
+// Back to Top button
+$("#back-to-top").hide();
+
+$(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 500) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+
+	// scrolls back up to 0px on click
+	$('#back-to-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 700);
+		return false;
+	});
+});
+
+$('.showReferences').click(function(e) {
+	e.preventDefault();
+  $('.references').fadeIn(500)
+  $('.showReferences').fadeOut(500)
+});
+
+// Sign up form slide in upon click. 
+var signupVisible = 0;
+$(".man, .woman, .fitpro").click(function(e) {
+	e.preventDefault();
+	if (signupVisible === 0) {
+		$("#signupForm").slideToggle(500);
+		signupVisible = 1;	
+		$(".select, .signupLinks").fadeOut(500);
+	} else {
+		$("#signupForm").slideToggle(500);
+		signupVisible = 0;
+		$(".select").fadeIn(500);
+	}
+});
+
+$("#nav").addClass("js");
+$("#nav").addClass("js").before('<div id="menu"><i class="icon-reorder"></i></div>');
+$("#menu").click(function(){
+	$("#nav").toggle();
+});
+$(window).resize(function(){
+	if(window.innerWidth > 768) {
+		$("#nav").removeAttr("style");
+	}
+});
+
+$("#video").fitVids();
